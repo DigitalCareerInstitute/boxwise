f90c3a01bab6c1e0a248f6a3c017ab8eb83bbc39
Rubens Pinheiro <rubenspgcavalcante@gmail.com>
2018-06-21 16:54:21 +0200

    Config generation now only needs the project ID

--- config-overrides.js
+++ config-overrides.js
@@ -1,3 +1,11 @@
+/**
+ * This function overrides the default webpack.config file
+ * provided by react-create-app.
+ * It will be called by react-app-rewired.
+ * @param {object} config
+ * @param {"production"|"development"} env
+ * @returns {object}
+ */
 module.exports = function override(config, env) {
   if (env === 'development') {
     config.devtool = "source-map";
